058bc2d9ad05eac3180948221d77a4f83b9db5ae
Neil-urk12 <neilvallecer12@gmail.com>
2024-11-29 19:02:07 +0800

    Adds JSDoc documentation to database methods
    Improves code readability and maintainability by adding detailed JSDoc documentation to all database methods.  This clarifies the purpose, parameters, and return values of each function.

--- src/db/dexiedb.js
+++ src/db/dexiedb.js
@@ -1,8 +1,24 @@
+/**
+ * @fileoverview The local database instance.
+ * All functions and methods are managed and implemented in this file.
+ * **/
+
 import Dexie from 'dexie';
 
+
+/**
+ * @class AppDatabase
+ * @description The local database instance.
+ * Manages all functions and methods for the database.
+ * @extends Dexie
+*/
 class AppDatabase extends Dexie {
+  /**
+   * @constructor
+   * @description Creates a new instance of the AppDatabase class.
+  */
   constructor() {
-    super('inventoryDb');
+    super('inventoryDb'); // Name of the database
 
     this.version(1).stores({
       // Inventory tables
@@ -30,29 +46,66 @@ class AppDatabase extends Dexie {
   }
 
   // Inventory Methods--------------------------------------------------
+  /**
+   * @async
+   * @method getAllItems
+   * @returns {Promise<Array>}
+   * @description Returns an array of all items in the database.
+  */
   async getAllItems() {
     return await this.items.toArray();
   }
-
+  /**
+   * @async
+   * @method addItem
+   * @param {Object} item
+   * @returns {Promise<Object>}
+   * @description Adds an item to the database.
+  */
   async addItem(item) {
     return await this.items.add(item);
   }
-
+  /**
+   * @async
+   * @method updateItem
+   * @param {*} id
+   * @param {*} changes
+   * @returns {Promise<Object>}
+   * @description Updates an item in the database.
+  */
   async updateItem(id, changes) {
     return await this.items.update(id, changes);
   }
-
+  /**
+   * @async
+   * @method deleteItem
+   * @param {*} id
+   * @returns {Promise<Object>}
+   * @description Deletes an item from the database.
+  */
   async deleteItem(id) {
     return await this.items.delete(id);
   }
-
+  /**
+   * @async
+   * @method addSale
+   * @param {Object} sale
+   * @returns {Promise<Object>}
+   * @description Adds a sale to the database.
+  */
   async addSale(sale) {
     return await this.sales.add({
       ...sale,
       date: new Date().toISOString()
     });
   }
-
+  /**
+   * @async
+   * @method addCashFlowTransaction
+   * @param {Object} transaction
+   * @returns {Promise<Object>}
+   * @description Adds a cash flow transaction to the database.
+  */
   async addCashFlowTransaction(transaction) {
     return await this.cashFlow.add({
       ...transaction,
@@ -61,45 +114,108 @@ class AppDatabase extends Dexie {
   }
 //--------------------------------------------------------------------
   // Contacts Methods
+  /**
+   * @async
+   * @method getAllContactCategories
+   * @returns {Promise<Array>}
+   * @description Returns an array of all contact categories in the database.
+  */
   async getAllContactCategories() {
     return await this.contactCategories.toArray();
   }
-
+  /**
+   * @async
+   * @method addContactCategory
+   * @param {Object} contactCategory
+   * @returns {Promise<Object>}
+   * @description Adds a contact category to the database.
+  */
   async addContactCategory(contactCategory) {
     return await this.contactCategories.add(contactCategory);
   }
-
+  /**
+   * @async
+   * @method updateContactCategory
+   * @param {*} contactCategoryId
+   * @param {*} changes
+   * @returns {Promise<Object>}
+   * @description Updates a contact category in the database.
+  */
   async updateContactCategory(contactCategoryId, changes) {
     return await this.contactCategories.update(contactCategoryId, changes);
   }
-
+  /**
+   * @async
+   * @method deleteContactCategory
+   * @param {*} contactCategoryId
+   * @returns {Promise<Object>}
+   * @description Deletes a contact category from the database.
+  */
   async deleteContactCategory(contactCategoryId) {
     // Delete all contacts in this contact category first
     await this.contactsList.where('categoryId').equals(contactCategoryId).delete();
     return await this.contactCategories.delete(contactCategoryId);
   }
-
+  /**
+   * @async
+   * @method getContactsByCategory
+   * @param {*} contactCategoryId
+   * @returns {Promise<Array>}
+   * @description Returns an array of contacts in a specific category.
+  */
   async getContactsByCategory(contactCategoryId) {
     return await this.contactsList.where('categoryId').equals(contactCategoryId).toArray();
   }
-
+  /**
+   * @async
+   * @method getAllContacts
+   * @returns {Promise<Array>}
+   * @description Returns an array of all contacts in the database.
+  */
   async getAllContacts() {
     return await this.contactsList.toArray();
   }
-
+  /**
+   * @async
+   * @method addContact
+   * @param {Object} contactPerson
+   * @returns {Promise<Object>}
+   * @description Adds a contact to the database.
+  */
   async addContact(contactPerson) {
     return await this.contactsList.add(contactPerson);
   }
-
+  /**
+   * @async
+   * @method updateContact
+   * @param {*} contactId
+   * @param {*} changes
+   * @returns {Promise<Object>}
+   * @description Updates a contact in the database.
+  */
   async updateContact(contactId, changes) {
     return await this.contactsList.update(contactId, changes);
   }
-
+  /**
+   * @async
+   * @method deleteContact
+   * @param {*} contactId
+   * @returns {Promise<Object>}
+   * @description Deletes a contact from the database.
+  */
   async deleteContact(contactId) {
     return await this.contactsList.delete(contactId);
   }
   //--------------------------------------------------------------------
   // Sync Methods
+  /**
+   * @async
+   * @method syncWithFirestore
+   * @param {Array} firestoreData
+   * @param {String} table
+   * @returns {Promise<Object>}
+   * @description Syncs data from a Firestore table to a Dexie table.
+  */
   async syncWithFirestore(firestoreData, table) {
     // Begin transaction
     return await this.transaction('rw', this[table], async () => {
@@ -110,7 +226,14 @@ class AppDatabase extends Dexie {
       await this[table].bulkAdd(firestoreData);
     });
   }
-
+  /**
+   * @async
+   * @method syncWithFirestoreSimple
+   * @param {Array} items
+   * @param {String} tableName
+   * @returns {Promise<Object>}
+   * @description Syncs data from an array to a Dexie table.
+  */
   async syncWithFirestoreSimple(items, tableName) {
     const table = this.table(tableName);
     await table.clear();
@@ -118,8 +241,12 @@ class AppDatabase extends Dexie {
   }
 
   //--------------------------------------------------------------------
-
-  // Utility Methods
+  /**
+   * @async
+   * @method clearAllTables
+   * @returns {Promise<Object>}
+   * @description Clears all tables in the database.
+  */
   async clearAllTables() {
     await this.transaction('rw',
       this.items,
@@ -145,4 +272,4 @@ class AppDatabase extends Dexie {
 }
 
 // Create and export a single instance
-export const db = new AppDatabase();
+export const db = new AppDatabase()
